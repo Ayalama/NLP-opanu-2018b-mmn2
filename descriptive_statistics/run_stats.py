bfbd4a741240c38d9f450e39f34cb303003cebdb
aymann <aymann@ebay.com>
2018-04-02 21:35:43 +0300

    arranged code

--- descriptive_statistics/run_stats.py
+++ descriptive_statistics/run_stats.py
@@ -14,7 +14,7 @@ def read_train_gold():
 # returned number of unique unigrams and total unigrams
 def count_unigrams_instances(data_set):
     uniq_unigrams = len(data_set.SEG.unique())
-    total_unigrams = len(data_set.SEG)
+    total_unigrams = data_set.shape[0]
     return uniq_unigrams, total_unigrams
 
 
@@ -35,7 +35,7 @@ def count_segment_tag_instances(data_set):
 # count the number of segment-tag pairs in the given data set
 # referred keys in the data set are 'SEG' and 'TAG'
 
-#what is the differnace between this ans segment-tag instances?!
+# what is the differnace between this ans segment-tag instances?!
 def count_segment_tag_types(data_set):
     return
 
@@ -44,50 +44,35 @@ def count_segment_tag_types(data_set):
 # referred keys in the data set are 'SEG' and 'TAG'
 def index_of_ambiguity(data_set):
     df = data_set[['SEG', 'TAG']]
-    df=df.drop_duplicates() # keep only uniq TAG+SEGMENT pairs
-    df= df.groupby('SEG').agg('count').reset_index() # count number of uniq TAGS per segment
-    return np.average(df.TAG) # return average of uniq tags per segment
+    df = df.drop_duplicates()  # keep only uniq TAG+SEGMENT pairs
+    df = df.groupby('SEG').agg('count').reset_index()  # count number of uniq TAGS per segment
+    return np.average(df.TAG)  # return average of uniq tags per segment
+
+
+def print_stats(dataframe):
+    unigrams_instances_uniq, unigrams_instances = count_unigrams_instances(dataframe)
+    segment_tag_instances = count_segment_tag_instances(dataframe)
+    unigrams_types = count_unigrams_types(dataframe)
+    # segment_tag_types = count_segment_tag_types(dataframe)
+    index_of_ambiguity_s = index_of_ambiguity(dataframe)
+    print "# number of unigram instances: {unigram_sntances} \n" \
+          "# number of unigram types: {unigram_types} \n" \
+          "# number of segment- tag instances: {seg_tag_instance} \n" \
+          "# number of segment- tag types: {seg_tag_typ} \n" \
+          "# index of ambiguity: {amb_idx} \n".format(unigram_sntances=unigrams_instances,
+                                                      unigram_types=unigrams_instances_uniq,
+                                                      seg_tag_instance=segment_tag_instances,
+                                                      seg_tag_typ=unigrams_types,
+                                                      amb_idx=index_of_ambiguity_s)
 
 
 if __name__ == '__main__':  # This is needed to allow multiprocessing in windows
     gold, train = read_train_gold()
-    gold_unigrams_instances,gold_unigrams_rows = count_unigrams_instances(gold)
-    gold_unigrams_types = count_unigrams_types(gold)
-    gold_segment_tag_instances = count_segment_tag_instances(gold)
-    gold_segment_tag_types = count_segment_tag_types(gold)
-    gold_index_of_ambiguity = index_of_ambiguity(gold)
-    print "Gold corpus statistics: \n" \
-          "# number of unigram instances: {} \n" \
-          "# number of unigram types: {} \n" \
-          "# number of segment- tag instances: {} \n" \
-          "# number of segment- tag types: {} \n" \
-          "# index of ambiguity: {} \n".format(gold_unigrams_instances, gold_unigrams_types, gold_segment_tag_instances,
-                                               gold_segment_tag_types, gold_index_of_ambiguity)
+    print "Gold corpus statistics: \n"
+    print_stats(gold)
 
-    train_unigrams_instances,train_unigrams_rows = count_unigrams_instances(train)
-    train_unigrams_types = count_unigrams_types(train)
-    train_segment_tag_instances = count_segment_tag_instances(train)
-    train_segment_tag_types = count_segment_tag_types(train)
-    train_index_of_ambiguity = index_of_ambiguity(train)
-    print "Train corpus statistics: \n" \
-          "# number of unigram instances: {} \n" \
-          "# number of unigram types: {} \n" \
-          "# number of segment- tag instances: {} \n" \
-          "# number of segment- tag types: {} \n" \
-          "# index of ambiguity: {} \n".format(train_unigrams_instances, train_unigrams_types,
-                                               train_segment_tag_instances, train_segment_tag_types,
-                                               train_index_of_ambiguity)
+    print "Train corpus statistics: \n"
+    print_stats(train)
 
-    all_unigrams_instances,train_unigrams_rows = count_unigrams_instances(pd.concat([train,gold]))
-    all_unigrams_types = count_unigrams_types(pd.concat([train,gold]))
-    all_segment_tag_instances = count_segment_tag_instances(pd.concat([train,gold]))
-    all_segment_tag_types = count_segment_tag_types(pd.concat([train,gold]))
-    all_index_of_ambiguity = index_of_ambiguity(pd.concat([train,gold]))
-    print "Entire corpus statistics: \n" \
-          "# number of unigram instances: {} \n" \
-          "# number of unigram types: {} \n" \
-          "# number of segment- tag instances: {} \n" \
-          "# number of segment- tag types: {} \n" \
-          "# index of ambiguity: {} \n".format(all_unigrams_instances, all_unigrams_types,
-                                               all_segment_tag_instances, all_segment_tag_types,
-                                               all_index_of_ambiguity)
+    print "Entire corpus statistics: \n"
+    print_stats(pd.concat([train, gold],ignore_index=True))
